5d3eb58d69e09c20f62d7b703c0e62f08ed03a45
Shera <shera.animelist@gmail.com>
2023-02-06 12:30:23 +0700

    vector_new

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ list = [10, 20, 30, 40, 50]
 
 vctr = np.array(list)
 
-vctr = np.array(list)
-
 print("List : ")
 print(vctr)
+
+v1 = np.array([2, 1, 2])
+v2 = np.array([3, 1, 5])
+
+print("List v1: ")
+print(v1)
+print("List v2:")
+print(v2)
+
+vctr_dot = v1.dot(v2)
+print("Dot product of two vector = ", vctr_dot)
